22d345ad7646fedea155c45121f80f6df8e935bc
reoy <mrtmrt15xn@yahoo.com.tw>
2014-08-21 23:09:01 +0800

    新增bigint library
    2014/07/01 03:01:51

---
+++ lib/bigint-2010.04.30/_BigIntegerLibrary.cc
@@ -0,0 +1,7 @@
+
+#include "_BigIntegerLibrary.hh"
+#include "BigInteger.cc"
+#include "BigIntegerAlgorithms.cc"
+#include "BigIntegerUtils.cc"
+#include "BigUnsigned.cc"
+#include "BigUnsignedInABase.cc"
